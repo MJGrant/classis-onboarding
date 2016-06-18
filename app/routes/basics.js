e7e3ff074a1151c6b0afdaf04b693140349cc06b
Mandi Grant <magrant@expedia.com>
2016-06-17 22:09:14 -0700

    First pass on models for a class and a user, got a continue button working, added a card, other small updates

--- app/routes/basics.js
+++ app/routes/basics.js
@@ -5,5 +5,24 @@ export default Ember.Route.extend({
   setupController() {
     let application = this.controllerFor('application');
     application.set('pageTitle', 'Basics');
+
+    this.controller.set('title', '');
+    this.controller.set('description', '');
+  },
+
+  actions: {
+    continue() {
+      console.log("continuing on to location and making a record");
+/*
+      this.store.createRecord('class', {
+        title: this.controller.get('title'),
+        description: this.controller.get('description')
+      });
+      */
+
+      //advance to location route
+      this.replaceWith('location');
+    }
   }
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  model() {
+      this.store.findRecord('class');
+  },
+  
   setupController() {
     let application = this.controllerFor('application');
     application.set('pageTitle', 'Classis');
