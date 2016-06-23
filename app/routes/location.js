df9030e790b10f8be46de75a588b86dd778c0419
Mandi Grant <magrant@expedia.com>
2016-06-22 22:19:37 -0700

    Added radio button add on, changed checkbox in location to two radio buttons, improved address form presentation

--- app/routes/location.js
+++ app/routes/location.js
@@ -2,26 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model() {
-    return this.store.findRecord('class', 1);
-  },
-
-  setupController(model) {
+  setupController() {
     let application = this.controllerFor('application');
     application.set('pageTitle', 'Location');
 
-    this.controller.set('clientLocation', model.get('clientLocation'));
+    this.controller.set('clientLocation', "yours");
   },
 
   actions: {
     continue() {
       console.log("back to index");
 
-      model.set('clientLocation', this.controller.get('clientLocation'));
+      //model.set('clientLocation', this.controller.get('clientLocation'));
 
       //advance to location route
       this.replaceWith('location');
+    },
+
+    locationToggled(choice) {
+      this.controller.set('clientLocation', choice);
     }
+
   }
 
 
