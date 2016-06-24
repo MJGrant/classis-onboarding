324e41b67343475da7c58bbd308d8d95ae78d571
Mandi Grant <magrant@expedia.com>
2016-06-24 14:36:08 -0700

    Improvements to main page presentation with cards and add a new class link

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-      //classes should already be in data, so just use peek 
+      //classes should already be in data, so just use peek
       return this.store.peekAll('class');
   },
 
